e5222fa55197d91de1bf5c997d4531b478d84cc1
Pan Alex <xhsiung@Panteki-Mac-Pro.local>
2017-07-06 13:20:15 +0800

    fix Utils

--- src/ios/Utils.swift
+++ src/ios/Utils.swift
@@ -46,15 +46,18 @@ class Utils {
         jsonArr.append( JSON( ["channel": owner!["m_id"].stringValue , "device":"mobile"] )  )
         
         for user in ( contactsDB["data"].array)! {
-            //ignore owner
-            if user["corps"] == -1 { continue }
+            //owner
+            if user["corps"] == -1 {
+                jsonArr.append( JSON(["channel": user["m_id"].stringValue,"device":"mobile"]) )
+                continue
+            }
             
-            //personal
+            //person
             if user["isgroup"] == 0 {
                 let inviteChann = Utils.getInviteChann(sid: owner!["m_id"].stringValue, tid: user["m_id"].stringValue)
                 jsonArr.append( JSON(["channel": inviteChann,"device":"mobile"]) )
                 
-                //group
+            //group
             }else if user["isgroup"] == 1 {
                 jsonArr.append( JSON(["channel": user["m_id"].stringValue,"device":"mobile"]) )
             }
@@ -190,15 +193,19 @@ class Utils {
     func restContactsAdd() {
         let jsonPack = getContactsPackJson()
         let resturl = "\(getServerURL())/contacts/add"
+        print(resturl)
+        
         DispatchQueue.global().async {
             Alamofire.request( resturl,method:.post
                 ,parameters:jsonPack.dictionary! ,headers:["Accept": "application/json"]).responseJSON { response in
                 //debugPrint(response)
                 //var jobj = JSON(["data":[:] ])
+                
                 switch response.result {
                 case .success(let value):
                     let _ = JSON(value)
                     print("restContactsAdd scucess")
+                    
                 case .failure(let error):
                     print(error)
                 }

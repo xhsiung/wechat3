344e0c39ec64a013c25f9ff8b5cd9933851f59b7
Pan Alex <xhsiung@Panteki-Mac-Pro.local>
2017-07-06 15:14:55 +0800

    fix

--- src/ios/AppDelegate.swift
+++ src/ios/AppDelegate.swift
@@ -40,7 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         print("applicationDidEnterBackground")
-        _exit(-1)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {

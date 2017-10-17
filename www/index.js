609a52c232b64cbbe75c9811bbec01a3b1507013
Pan Alex <xhsiung@Panteki-Mac-Pro.local>
2017-10-17 11:21:57 +0800

    fix index.js

--- www/index.js
+++ www/index.js
@@ -32,6 +32,7 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+	/*
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -40,6 +41,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+	*/
     }
 };
 
